ed9a898142fe5a700d34a27cb0135c8a8b376ed6
ne_andrey <sorokinac@gmail.com>
2018-02-03 01:03:05 +0500

    first_commit

--- pelicanconf.py
+++ pelicanconf.py
@@ -4,7 +4,7 @@ from __future__ import unicode_literals
 
 AUTHOR = 'andrey'
 SITENAME = '123'
-SITEURL = 'https://github.com/neandrey/neandreyblog/tree/master/output'
+SITEURL = 'https://github.com/neandrey/neandreyblog/tree/master/output/index.html'
 
 PATH = 'content'
 
