960fabc8df1c80af23f686db34986d40f905e4b8
ne_andrey <sorokinac@gmail.com>
2018-02-04 02:25:38 +0500

    local_host

--- pelicanconf.py
+++ pelicanconf.py
@@ -8,31 +8,73 @@ AUTHOR = u'ne_andrey'
 SITENAME = u'123'
 SITEURL = 'https://neandreyblog.herokuapp.com/'
 
+# Uncomment following line if you want document-relative URLs when developing
+RELATIVE_URLS = True
+
+
+# Define some project paths that have special meanings in Pelican
 PATH = 'content'
+PAGE_PATHS = ['pages']
+STATIC_PATHS = ['images']
 
-TIMEZONE = 'Europe/Paris'
-
+# Internationalization settings
+TIMEZONE = 'Europe/London'
 DEFAULT_LANG = u'en'
 
+# Feed generation is usually not desired when developing
 FEED_ALL_ATOM = None
 CATEGORY_FEED_ATOM = None
 TRANSLATION_FEED_ATOM = None
 AUTHOR_FEED_ATOM = None
 AUTHOR_FEED_RSS = None
 
+# Gather as much metadata as possible from the file system
+USE_FOLDER_AS_CATEGORY = True
+FILENAME_METADATA = '(?P<date>\d{4}-\d{2}-\d{2})_(?P<slug>.*)'
+DEFAULT_DATE = 'fs'
+# Set some defaults§
+DEFAULT_CATEGORY = 'ramblings'
+DEFAULT_DATE_FORMAT = '%a %d %B %Y'
+DEFAULT_PAGINATION = 10
 
+
+# Theme
+THEME = "themes/pure-single"
+COVER_IMG_URL = '/images/sidebar.jpg'
+TAGLINE = "Man of few words..."
+
+
+# Configure the site menu
+# Fixed menu entries
+MENUITEMS = [
+    ('Archives', '/archives.html')
+]
+# Dynamic menu entries§
+DISPLAY_CATEGORIES_ON_MENU = False
+DISPLAY_PAGES_ON_MENU = True
+
+# Social widget
+SOCIAL = (('twitter', 'https://twitter.com/geospaced'),
+          ('github', "https://github.com/vanceb")
+          )
+
+# Photos plugin
+PLUGIN_PATHS = ['../pelican-plugins']
+PLUGINS = ['photos']
+PHOTO_LIBRARY = "~/sites/photos"
+PHOTO_GALLERY = (1024, 768, 80)
+PHOTO_ARTICLE = (760,506, 80)
+PHOTO_THUMB = (192,144,60)
 
 # Blogroll
-LINKS = (
-    ('GitHub', 'https://github.com/CraigJPerry'),
-)
+#LINKS = (
+#    ('GitHub', 'https://github.com/CraigJPerry'),
+#)
 
-SOCIAL = (
-    ('Twitter', 'https://twitter.com/CraigJPerry'),
-)
+#SOCIAL = (
+#    ('Twitter', 'https://twitter.com/CraigJPerry'),
+#)
 
-DEFAULT_PAGINATION = False
-RELATIVE_URLS = True
 #THEME = "themes/pelican-svbhack"
 #USER_LOGO_URL = '/img/me-small.jpg'
 #TAGLINE = "Unix systems administrator. Python / Java / Web developer."
