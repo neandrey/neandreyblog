f3e4cba41537214b0ca475b6c00351deb754d4d7
ne_andrey <sorokinac@gmail.com>
2018-02-04 01:46:46 +0500

    local_host

--- pelicanconf.py
+++ pelicanconf.py
@@ -4,7 +4,7 @@ from __future__ import unicode_literals
 
 
 
-AUTHOR = u'andrey'
+AUTHOR = u'ne_andrey'
 SITENAME = u'123'
 SITEURL = 'https://neandreyblog.herokuapp.com/'
 
@@ -33,8 +33,8 @@ SOCIAL = (
 
 DEFAULT_PAGINATION = False
 RELATIVE_URLS = True
-THEME = "themes/pelican-svbhack"
+#THEME = "themes/pelican-svbhack"
 #USER_LOGO_URL = '/img/me-small.jpg'
-TAGLINE = "Unix systems administrator. Python / Java / Web developer."
+#TAGLINE = "Unix systems administrator. Python / Java / Web developer."
 # Uncomment following line if you want document-relative URLs when developing
 #RELATIVE_URLS = True
