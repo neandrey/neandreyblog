4eb4fab408feea84951bccd2d3112bb3c4a259d0
ne_andrey <sorokinac@gmail.com>
2018-02-03 23:16:20 +0500

    local_host

--- pelicanconf.py
+++ pelicanconf.py
@@ -32,7 +32,8 @@ LINKS = (('Pelican', 'http://getpelican.com/'),
 SOCIAL = (('You can add links in your config file', '#'),
           ('Another social link', '#'),)
 
-DEFAULT_PAGINATION = 10
-
+DEFAULT_PAGINATION = False
+RELATIVE_URLS = True
+THEME = 'themes/pelican-svbhack'
 # Uncomment following line if you want document-relative URLs when developing
 #RELATIVE_URLS = True
