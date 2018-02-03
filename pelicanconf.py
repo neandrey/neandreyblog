019e1dc8ea8ac38f166a2695ada392eb2c530a3e
ne_andrey <sorokinac@gmail.com>
2018-02-03 17:34:47 +0500

    first_commit

--- pelicanconf.py
+++ pelicanconf.py
@@ -3,7 +3,7 @@
 from __future__ import unicode_literals
 
 
-THEME = '~/pyProject/pelican-svbhack/output/theme'
+THEME = '~/pyProject/pelican-svbhack/'
 AUTHOR = 'andrey'
 SITENAME = '123'
 SITEURL = 'https://neandreyblog.herokuapp.com/'
