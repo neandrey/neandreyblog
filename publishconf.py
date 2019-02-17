c1b3bfe9d0ac19c0632bcac608a1ca9db7d99e96
ne_andrey <sorokinac@gmail.com>
2019-02-17 13:17:37 +0500

    YouCompleteMe_1

--- publishconf.py
+++ publishconf.py
@@ -17,7 +17,7 @@ RELATIVE_URLS = False
 FEED_ALL_ATOM = 'feeds/all.atom.xml'
 CATEGORY_FEED_ATOM = 'feeds/%s.atom.xml'
 
-#CATEGORY_FEED_ATOM|format(slug = category.slug)
+#CATEGORY_FEED_ATOM = 'feeds/{slug}.atom.xml'
 
 DELETE_OUTPUT_DIRECTORY = True
 
